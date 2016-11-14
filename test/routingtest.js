821d8e544f3da94df262f6cad2ca2c6331bcb035
Capricorn397 <chrisjcarr4@hotmail.co.uk>
2016-11-14 12:01:43 +0000

    Added fully working pong test and also added a description of what each test will do

--- test/routingtest.js
+++ test/routingtest.js
@@ -1,2 +1,3 @@
 'use strict'
 const assert = require('assert')
+//It will test the routing file for whether all api routes are accessible and return an expected item
--- test/userauthtest.js
+++ test/userauthtest.js
@@ -1,2 +1,3 @@
 'use strict'
 const assert = require('assert')
+//It will test whether the user authentication is working
